5953f15f0d17999ebf9c07d21ac7941c4e59a8e5
Tahseen K. Kakar <tahseen.khan.kakar@gmail.com>
2026-03-12 23:08:21 +0500

    Initial Commit

---
+++ Harbor/HarborApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarborApp.swift
+//  Harbor
+//
+//  Created by Tahseen K. Kakar on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HarborApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
