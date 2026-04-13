2707aefb65c78d56601b561dd3aa8cb23864ff6b
Tahseen K. Kakar <tahseen.khan.kakar@gmail.com>
2026-04-13 23:33:40 +0500

    Handle torrent file opens through URL callbacks

--- Harbor/App/HarborAppDelegate.swift
+++ Harbor/App/HarborAppDelegate.swift
@@ -2,10 +2,19 @@ import AppKit
 
 @MainActor
 final class HarborAppDelegate: NSObject, NSApplicationDelegate {
+    func application(_ application: NSApplication, open urls: [URL]) {
+        handleOpenRequest(urls)
+    }
+
     func application(_ sender: NSApplication, openFiles filenames: [String]) {
         let urls = filenames.map { URL(fileURLWithPath: $0) }
-        let didHandle = ExternalTorrentOpenCoordinator.shared.receive(urls: urls)
+        let didHandle = handleOpenRequest(urls)
 
         sender.reply(toOpenOrPrint: didHandle ? .success : .failure)
+    }
+
+    @discardableResult
+    private func handleOpenRequest(_ urls: [URL]) -> Bool {
+        ExternalTorrentOpenCoordinator.shared.receive(urls: urls)
     }
 }
